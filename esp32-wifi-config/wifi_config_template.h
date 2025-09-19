c39300dc3ef6e086d2cb310700d0d92148959e45
John Ajera <37360952+jajera@users.noreply.github.com>
2025-09-19 09:41:14 +0000

    feat: initial commit
    first release

---
+++ esp32-wifi-config/wifi_config_template.h
@@ -0,0 +1,12 @@
+// WiFi Configuration Template
+// Copy this file to wifi_config.h and add your credentials
+// wifi_config.h is gitignored for security
+
+#ifndef WIFI_CONFIG_H
+#define WIFI_CONFIG_H
+
+// Change these to your WiFi credentials
+const char *WIFI_SSID = "YourWiFiNetwork";
+const char *WIFI_PASSWORD = "YourWiFiPassword";
+
+#endif
